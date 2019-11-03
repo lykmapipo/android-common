a12f4ee317703b7d6a5867acffa7a0f869fb1a08
lykmapipo <lallyelias87@gmail.com>
2019-11-03 12:31:14 +0300

    feat: make provider abstract to provide defaults

--- library/src/main/java/com/github/lykmapipo/common/provider/Provider.java
+++ library/src/main/java/com/github/lykmapipo/common/provider/Provider.java
@@ -1,28 +1,49 @@
 package com.github.lykmapipo.common.provider;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.collection.ArraySet;
+
+import java.util.Set;
 
 /**
  * Provides ability to retrieve current application information.
  *
  * @since 0.1.0
  */
-public interface Provider {
+public abstract class Provider {
     /**
      * Returns application {@link Context}.
      * <p>
      * {@link Context#getApplicationContext()}
      */
     @NonNull
-    Context getApplicationContext();
+    public abstract Context getApplicationContext();
 
     /**
      * Returns application debug state from build config
      *
-     * @return if build is debug
+     * @return if build is debug, default to true
      */
     @NonNull
-    Boolean isDebug();
+    public Boolean isDebug() {
+        return true;
+    }
+
+    /**
+     * Returns application ignored log levels
+     *
+     * @return if build is debug, default are {@link Log#VERBOSE},
+     * {@link Log#DEBUG}, {@link Log#INFO}
+     */
+    public Set<Integer> ignoredLogLevels() {
+        ArraySet<Integer> ignored = new ArraySet<Integer>();
+        ignored.add(Log.VERBOSE);
+        ignored.add(Log.DEBUG);
+        ignored.add(Log.INFO);
+        return ignored;
+    }
+
 }
